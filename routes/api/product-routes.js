9189f9892c42eafefda539eb4cf884a56d652c6f
devasmi <impokhrelasmita5@gmail.com>
2023-02-06 07:51:52 -0500

    product and tags route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,21 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, model: Tag }],
+    });
+    if (!product) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -97,8 +109,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productDel = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productDel) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    };
+    res.send(200);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
